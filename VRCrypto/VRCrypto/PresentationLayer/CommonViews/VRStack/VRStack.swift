0c2a4d9aab04fb0832d22907a3cfe9a8f9bac1cc
Vishank Raghav <vishank13@gmail.com>
2025-01-30 21:20:39 +0530

    Added: 1. CoinList Response 2. Reusable VRStack 3. Decoder for JSON data 4. CacheImage View

---
+++ VRCrypto/VRCrypto/PresentationLayer/CommonViews/VRStack/VRStack.swift
@@ -0,0 +1,32 @@
+//
+//  VRStack.swift
+//  VRCrypto
+//
+//  Created by Vishank Raghav on 30/01/25.
+//
+
+import SwiftUI
+
+struct VRStack<Content: View>: View {
+    
+    let content: Content
+    
+    init(@ViewBuilder content: () -> Content) {
+        self.content = content()
+    }
+    
+    var body: some View {
+        ZStack {
+            Color(.background).ignoresSafeArea()
+            
+            content
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+#Preview {
+    VRStack {
+        Text("Hello")
+    }
+}
