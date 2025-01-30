075befb89f793c9bcedfdcd5fceaa452e35c111a
Vishank Raghav <vishank13@gmail.com>
2025-01-30 19:27:22 +0530

    Project Setup

---
+++ VRCrypto/VRCrypto/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  VRCrypto
+//
+//  Created by Vishank Raghav on 30/01/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    
+    var body: some View {
+        Text("Hello World!!")
+    }
+    
+}
+
+#Preview {
+    HomeView()
+}
---
+++ VRCrypto/VRCrypto/VRCryptoApp.swift
@@ -0,0 +1,20 @@
+//
+//  VRCryptoApp.swift
+//  VRCrypto
+//
+//  Created by Vishank Raghav on 30/01/25.
+//
+
+import SwiftUI
+
+@main
+struct VRCryptoApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            NavigationStack {
+                HomeView()
+            }
+        }
+    }
+}
